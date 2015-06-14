57e1e3db0070e7311af590c129434e02afb48efa
sekuri <shukri@gmail.com>
2015-06-14 16:27:37 -0400

    minor

--- .emacs.d_init.el
+++ .emacs.d_init.el
@@ -11,25 +11,14 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ )
  '(global-visual-line-mode t)
- '(read-mail-command (quote gnus)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(autoload 'applescript-mode "applescript-mode"
-  "Major mode for editing AppleScript source." t)
-(add-to-list 'auto-mode-alist '("\\.applescript$" . applescript-mode))
-(add-to-list 'auto-mode-alist '("\\.scrpt$" . applescript-mode))
-
-
-;; gnus+davmail bug, so I have to use pop3 for davmail
-;; http://permalink.gmane.org/gmane.emacs.gnus.general/83301
-;; but delete all the mails on server is scary
-;;(setq pop3-leave-mail-on-server t)
-
-;;(setq gnus-secondary-select-methods '((nnml "Exponent"))
-;;       mail-sources '((imap :server "127.0.0.1"
-;;                           :user "ssouri")))
+(require 'apples-mode)
+(add-to-list 'auto-mode-alist '("\\.\\(applescri\\|sc\\)pt\\'" . apples-mode))
+(global-set-key (kbd "s-w") 'save-buffers-kill-emacs)
